89a9c87eda39043b2da3f3385ed32417134856f3
nanou <anael.chardan@gmail.com>
2025-03-29 11:15:33 +0100

    bun server

--- promptfoo/index.ts
+++ promptfoo/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+const port = 3555;
+
+Bun.serve({
+  port,
+  // `routes` requires Bun v1.2.3+
+  routes: {
+    // Per-HTTP method handlers
+    "/test-prompt": {
+      POST: async (req) => {
+        const body = await req.json();
+        console.log(body);
+        return new Response(JSON.stringify(body));
+      },
+    },
+  },
+
+  // (optional) fallback for unmatched routes:
+  // Required if Bun's version < 1.2.3
+  fetch(req) {
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log(`Server is running on http://localhost:${port}`);
