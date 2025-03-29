3e634a6e1fb00bf3bca3b7914ea9d3b758fa5eac
robinbdru <robinbeaudru@icloud.com>
2025-03-30 00:06:57 +0100

    css

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,37 +1,33 @@
 <script lang="ts">
 	import Button from "$lib/components/ui/button/button.svelte";
-	import { ArrowRight, BadgeEuroIcon, BriefcaseMedical, Building2, PlaneTakeoff, Smile } from "@lucide/svelte";
+	import {
+		ArrowRight,
+		BadgeEuroIcon,
+		BriefcaseMedical,
+		Building2,
+		PlaneTakeoff,
+		Smile,
+		Sparkles,
+	} from "@lucide/svelte";
 
 	const samples = $state([
 		{
 			id: "health",
 			icon: BriefcaseMedical,
-			label: "Medical Assistant",
-			description: "A medical assistant that can provide information about health conditions and treatments.",
-		},
-		{
-			id: "legal",
-			icon: Building2,
-			label: "Legal Assistant",
-			description: "A legal assistant that can provide information about laws and regulations.",
-		},
-		{
-			id: "finance",
-			icon: BadgeEuroIcon,
-			label: "Financial Advisor",
-			description: "A financial advisor that can provide information about investments and savings.",
-		},
-		{
-			id: "travel",
-			icon: PlaneTakeoff,
-			label: "Travel Guide",
-			description: "A travel guide that can provide information about destinations and travel tips.",
+			label: "Medical Agent",
+			description: "Provides health information and treatments.",
 		},
 		{
 			id: "education",
 			icon: Smile,
-			label: "Educational Assistant",
-			description: "An educational assistant that can provide information about courses and learning resources.",
+			label: "Educational Agent",
+			description: "Provides info on courses and learning resources.",
+		},
+		{
+			id: "legal",
+			icon: Building2,
+			label: "Legal Agent",
+			description: "Provides info on laws and regulations.",
 		},
 	]);
 </script>
@@ -44,17 +40,29 @@
 	/>
 </svelte:head>
 
-<div class="h-full overflow-hidden flex flex-col items-center justify-between">
+<div class="h-full overflow-hidden flex flex-col items-center">
 	<div class="w-full max-w-4xl text-balance grid gap-4 text-center pt-16">
 		<h1 class="text-5xl">Bullet proof your system's prompts.</h1>
 		<p class="text-3xl text-muted-foreground font-light">
-			Improve prompt engineering skills and learn strategies to protect and educate against agent hacking.
+			Learn and implement strategies to protect and educate against agent hacking.
 		</p>
+		<div class="mt-8 grid gap-4">
+			<textarea
+				class="w-full font-mono border bg-sidebar rounded p-4"
+				placeholder="Enter your system prompt here..."
+				rows={4}
+				cols={50}
+			></textarea>
+			<Button class="bg-indigo-600 text-white hover:bg-indigo-700 transition-colors duration-150 ease-in-out">
+				Generate report
+				<Sparkles class="h-6 ml-2" />
+			</Button>
+		</div>
 	</div>
 
-	<div class="flex flex-col gap-2 justify-center">
-		<p class="text-muted-foreground text-center text-lg">some examples</p>
-		<div class="grid grid-cols-3 gap-4 p-4 max-w-5xl">
+	<div class="flex flex-col gap-2 justify-center mt-12">
+		<p class="text-muted-foreground text-center text-sm">some examples</p>
+		<div class="grid grid-cols-3 gap-4 max-w-4xl">
 			{#each samples as sample}
 				{@const Icon = sample.icon}
 				<div class="border rounded bg-sidebar p-6 justify-between flex flex-col gap-4">
@@ -67,7 +75,7 @@
 								</p>
 							</div>
 						</div>
-						<p class="text-sm text-muted-foreground my-4 text-balance">
+						<p class="text-sm text-muted-foreground my-2 text-balance">
 							{sample.description}
 						</p>
 					</div>
