1d164f97f6e8a33f29530ff6de1ed11c25377e6e
robinbdru <robinbeaudru@icloud.com>
2025-03-29 10:58:10 +0100

    added shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script>
+	import { toast } from "svelte-sonner";
 	import Button from "$lib/components/ui/button/button.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button>Test</Button>
+<Button
+	onclick={() => {
+		toast.info("Hello world");
+	}}
+>
+	Test
+</Button>
