308e6af7ce24701afa25117a5877d1ae309bb432
nanou <anael.chardan@gmail.com>
2025-03-30 15:05:52 +0200

    github stars

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -137,77 +137,87 @@
 </Dialog.Root>
 
 <div class="h-full flex flex-col items-center">
-	<div class="w-full max-w-4xl text-balance grid gap-4 text-center pt-16">
-		<h1 class="text-5xl">Bulletproof your system’s prompts.</h1>
-		<p class="text-3xl text-muted-foreground font-light">
-			Learn and implement strategies to protect and educate against agent hacking.
-		</p>
-		<div class="mt-8 grid gap-4">
-			<textarea
-				class="w-full font-mono border bg-sidebar rounded p-4"
-				placeholder="Enter your system prompt here..."
-				rows={4}
-				cols={50}
-				bind:value={prompt}
-			></textarea>
-			<Button
-				disabled={!prompt}
-				onclick={navigateOrDialog}
-				class="bg-indigo-600 text-white hover:bg-indigo-700 transition-colors duration-150 ease-in-out"
-			>
-				Generate report
-				<Sparkles class="h-6 ml-2" />
-			</Button>
-		</div>
-	</div>
+  <div class="w-full max-w-4xl text-balance grid gap-4 text-center pt-16">
+    <h1 class="text-5xl">Bulletproof your system’s prompts.</h1>
+    <p class="text-3xl text-muted-foreground font-light">
+      <strong>Learn</strong> and implement strategies to protect and educate against agent
+      hacking.
+    </p>
+    <div class="mt-8 grid gap-4">
+      <textarea
+        class="w-full font-mono border bg-sidebar rounded p-4"
+        placeholder="Enter your system prompt here..."
+        rows={4}
+        cols={50}
+        bind:value={prompt}
+      ></textarea>
+      <Button
+        disabled={!prompt}
+        onclick={navigateOrDialog}
+        class="bg-indigo-600 text-white hover:bg-indigo-700 transition-colors duration-150 ease-in-out"
+      >
+        Generate report
+        <Sparkles class="h-6 ml-2" />
+      </Button>
+    </div>
+  </div>
 
-	<div class="flex flex-col gap-2 justify-center mt-12">
-		<p class="text-muted-foreground text-center text-sm">
-			Our bank of prompts (+10000 examples available in premium)
-		</p>
-		<div class="grid grid-cols-3 gap-4 max-w-4xl">
-			{#each samples as sample}
-				{@const Icon = sample.icon}
-				<div class="border rounded bg-sidebar p-6 justify-between flex flex-col gap-4">
-					<div>
-						<div class="flex items-start gap-4">
-							<Icon class="h-5 w-5 shrink-0 mt-0.5" />
-							<div>
-								<p class="font-semibold">
-									{sample.label}
-								</p>
-							</div>
-						</div>
-						<p class="text-sm text-muted-foreground my-2 text-balance">
-							{sample.description}
-						</p>
-					</div>
-					<Button
-						variant="outline"
-						class="max-w-max"
-						onclick={() => {
-							prompt = sample.system_prompt;
-							promptId = sample.id;
-						}}
-					>
-						Test with this agent
-						<ArrowRight class="h-4 ml-2" />
-					</Button>
-				</div>
-			{/each}
-		</div>
-	</div>
+  <div class="flex flex-col gap-2 justify-center mt-12">
+    <p class="text-muted-foreground text-center text-xl">
+      Our bank of prompts
+    </p>
+    <p class="text-muted-foreground text-center text-xl font-semibold">
+      (+1000 examples available in premium)
+    </p>
+    <div class="grid grid-cols-3 gap-4 max-w-4xl">
+      {#each samples as sample}
+        {@const Icon = sample.icon}
+        <div
+          class="border rounded bg-sidebar p-6 justify-between flex flex-col gap-4"
+        >
+          <div>
+            <div class="flex items-start gap-4">
+              <Icon class="h-5 w-5 shrink-0 mt-0.5" />
+              <div>
+                <p class="font-semibold">
+                  {sample.label}
+                </p>
+              </div>
+            </div>
+            <p class="text-sm text-muted-foreground my-2 text-balance">
+              {sample.description}
+            </p>
+          </div>
+          <Button
+            variant="outline"
+            class="max-w-max"
+            onclick={() => {
+              prompt = sample.system_prompt;
+              promptId = sample.id;
+            }}
+          >
+            Test with this agent
+            <ArrowRight class="h-4 ml-2" />
+          </Button>
+        </div>
+      {/each}
+    </div>
+  </div>
 
-	<div class="py-24 flex flex-col items-center justify-center gap-4 max-w-4xl w-full">
-		<p class="text-xl font-semibold text-muted-foreground">Main vulnerabilities to protect against</p>
-		<div class="grid grid-cols-3 gap-4 w-full">
-			{#each hacks as hack}
-				<div class="border bg-white p-4 rounded">
-					<img
-						class="h-24 object-cover object-center rounded w-full"
-						src={hack.url}
-						alt=""
-					/>
+  <div
+    class="py-24 flex flex-col items-center justify-center gap-4 max-w-4xl w-full"
+  >
+    <p class="text-2xl font-semibold text-red-600">
+      Main vulnerabilities to protect against and learn from
+    </p>
+    <div class="grid grid-cols-3 gap-4 w-full">
+      {#each hacks as hack}
+        <div class="border bg-white p-4 rounded">
+          <img
+            class="h-24 object-cover object-center rounded w-full"
+            src={hack.url}
+            alt=""
+          />
 
 					<p class="test-sm font-bold mt-4">
 						{hack.label}
